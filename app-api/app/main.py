498987cb2b3a7f813df06a0f87fdb609abbc1524
Seven <addhome2001@yahoo.com.tw>
2019-03-16 18:09:16 +0800

    sperate __ini__.py and main,py

---
+++ app-api/app/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+from flask-sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+CORS(app)
+
+current_counts = 0
+
+@app.route('/ping')
+def index():
+  return 'pong'
+
+@app.route('/counts', methods=['GET', 'PUT'])
+def counts():
+  global current_counts
+
+  if request.method == 'PUT':
+    request_body = request.get_json()
+    current_counts = request_body.get('counts', 0)
+
+  response = jsonify(counts=current_counts)
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  return response
+
+@app.errorhandler(404)
+def not_found(error):
+  return 'Not Found!'
